8ca138fe4d026b36b4e48b04a8a58ded4a4a44af
Kniffen <kniffen@knifftech.org>
2023-01-08 15:00:33 +0100

    Update index.ts

--- src/info/index.ts
+++ src/info/index.ts
@@ -1,14 +1,12 @@
-import { selectedCell } from "../grid"
+import { selectedCell } from '../grid'
+import tools from '../tools/tools'
 
 const container = document.querySelector('.info') as HTMLElement
 
 export const update = function() {
-  container.innerText = `Selector
-  Column: ${selectedCell.column}
-  Row: ${selectedCell.row}
-  
-  Crop
-  Name: ${selectedCell.ref.name}
-  Stage: ${((selectedCell.ref.stage + 1) / selectedCell.ref.stages.length * 100).toFixed()}%
+  container.innerText = `
+  Crop: ${selectedCell.ref.name}
+  Allowed tools: ${tools.filter(tool => tool.usedOn.includes(selectedCell.ref.type)).map(tool => tool.name).join(', ')}
+  Growth: ${((selectedCell.ref.stage + 1) / selectedCell.ref.stages.length * 100).toFixed()}%
   `
 }
